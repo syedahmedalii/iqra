1763f6d65a9f84337e0b4be438214122aef670d1
syedahmedalii <syedahmedali08@gmail.com>
2022-01-01 03:38:42 +0500

    hi

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const pdfParse = require('pdf-parse')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
 app.use(cors())
 app.use(fileUpload())
 
@@ -20,5 +19,6 @@ app.post('/extract', (req, res)=>{
 
 })
 // app.use('/', express.static('public'))
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 8080);
